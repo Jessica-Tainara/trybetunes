d17d1d8664dd431c6332d58b1936c23783d60f10
jessica <jessicathaynnara@gmail.com>
2022-02-03 17:09:46 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Content from './components/Content';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <div>
+          <p>TrybeTunes</p>
+          <nav>
+            <Link to="/">Login</Link>
+            <Link to="/search">Buscar</Link>
+            <Link to="/album/:id">Album</Link>
+            <Link to="/favorites">Favoritos</Link>
+            <Link to="/profile">Perfil</Link>
+            <Link to="/profile/edit">Editar Perfil</Link>
+          </nav>
+          <Content />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
