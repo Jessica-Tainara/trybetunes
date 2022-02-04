a2f3c55ba1e0d6d7ff285ab88f525ba2c37485f4
jessica <jessicathaynnara@gmail.com>
2022-02-04 15:47:19 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -7,28 +10,62 @@ class Search extends React.Component {
 
     this.state = {
       artista: '',
+      nFound: '',
+      albuns: [],
+
     };
   }
 
   render() {
-    const { artista } = this.state;
+    const { artista, loading, hasAlbum, found, albuns, nFound } = this.state;
+    const form = (
+      <form>
+        <input
+          data-testid="search-artist-input"
+          onChange={ ({ target }) => { this.setState({ artista: target.value }); } }
+          value={ artista }
+        />
+        <button
+          type="submit"
+          disabled={ artista.length <= 1 }
+          onClick={ async () => {
+            this.setState({ loading: true });
+            await searchAlbumsAPI(artista)
+              .then((data) => {
+                this.setState({
+                  loading: false,
+                  hasAlbum: data.length > 0,
+                  found: `Resultado de álbuns de: ${artista}`,
+                  artista: '',
+                  albuns: data,
+                  nFound: 'Nenhum álbum foi encontrado',
+                });
+              });
+          } }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
+      </form>
+    );
+
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Buscar</h1>
-        <form>
-          <input
-            data-testid="search-artist-input"
-            onChange={ ({ target }) => { this.setState({ artista: target.value }); } }
-          />
-          <button
-            type="submit"
-            disabled={ artista.length <= 1 }
-            data-testid="search-artist-button"
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading ? <Loading /> : form}
+        {hasAlbum ? <p>{found}</p> : <p>{nFound}</p>}
+        <section>
+          {albuns.map(({ collectionId, collectionName }) => (
+            <Link
+              data-testid={ `link-to-album-${collectionId}` }
+              to={ `album/${collectionId}` }
+              key={ collectionId }
+            >
+              {collectionName}
+            </Link>
+          ))}
+        </section>
       </div>
     );
   }
