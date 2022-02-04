6fcf0a65d7885aad82baa9e1ff68afad284ee0c7
jessica <jessicathaynnara@gmail.com>
2022-02-03 22:09:10 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,33 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artista: '',
+    };
+  }
+
   render() {
+    const { artista } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Buscar</h1>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            onChange={ ({ target }) => { this.setState({ artista: target.value }); } }
+          />
+          <button
+            type="submit"
+            disabled={ artista.length <= 1 }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
